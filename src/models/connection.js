74e99dd24c2f0a500a573e8cb261ea5362bed9ef
eversondias <eversondias@pop-os.localdomain>
2023-04-27 17:39:40 -0300

    fixed gothub

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,10 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'store_manager_db',
-  user: 'root',
-  port: 3306,
-  password: 'password',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
 });
 
 module.exports = connection;
